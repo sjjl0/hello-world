0b56b86dd1208f5e1a3f1f5885d48c9baeb1f394
sjjl0 <43606858+sjjl0@users.noreply.github.com>
2018-10-28 21:17:49 +0800

    Update equation1.c

--- equation1.c
+++ equation1.c
@@ -1,3 +1,24 @@
-//求解一元二次方程的根
-//查阅sqrt()函数
-//注意有没有解
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	printf("输入一元二次方程各项系数求解方程\n");
+	//a*x*x+b*x+c==0
+	double a, b, c, x, y, z, o;
+	scanf("%lf%lf%lf", &a, &b, &c);
+	z = b * b - 4 * a*c;
+	if (z < 0)
+	{
+		printf("该方程无实数根\n");
+	}
+	else
+	{
+		o = sqrt(z);
+		x = (-b + o) / (2 * a);
+		y = (-b - o) / (2 * a);
+		printf("该方程第一个解为%lf\n", x);
+		printf("该方程第二个解为%lf\n", y);
+	}
+	getchar();
+	return 0;
+}
