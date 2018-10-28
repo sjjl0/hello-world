22fa35b40e7688a5bd8f09662cb970b102308efa
sjjl0 <43606858+sjjl0@users.noreply.github.com>
2018-10-28 21:16:27 +0800

    Update equation.c

--- equation.c
+++ equation.c
@@ -1,4 +1,26 @@
-//求解如下方程组
-//ax + by = c
-//dx + ey = f
-//注意输入/输出，数据类型
+#include <stdio.h>
+#include <math.h>
+int main(void)
+{
+	printf("输入二元一次方程各项系数以及方程的值求解方程\n");
+	//a*x+b*y==c
+	//d*x+e*y==f
+	double a, b, c, d, e, f, o, p, q, x, y;
+	scanf("%lf%lf%lf%lf%lf%lf", &a, &b, &c, &d, &e, &f);
+	o = a * e - b * d;
+	if (o < 0)
+	{
+		printf("该方程无解\n");
+	}
+	else
+	{
+		p = c * e - b * f;
+		q = a * f - c * d;
+		x = p / o;
+		y = q / o;
+		printf("x的值为%lf\n", x);
+		printf("y的值为%lf\n", y);
+	}
+	getchar();
+	return 0;
+}
