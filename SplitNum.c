8a262034cc210dbb3dc9c3b25c6014d3e68f805f
“sjjl0” <xfhy437945405@iCloud.com>
2018-12-06 21:22:49 +0800

    1

--- SplitNum.c
+++ SplitNum.c
@@ -1,5 +1,34 @@
-//输入整数，将每位数字打印出来（从左向右打印，空格区分）
-//2018/11/14 更新
-int main() {
-    //to do sth
+#include <stdio.h>
+int main()
+{
+	int a,b[100];
+	int i;
+	int j;
+	int h=0;
+	int c;
+	scanf ("%d",&a);
+	for(i=0;i<100;i++)
+		b[i]=0;
+	while(a>0)
+	{
+		b[h]=a%10;
+		a=a/10;
+		h++;
+	}
+	for(i=0;i<100;i++)
+		for(j=0;j<i-1;j++)
+		{
+			if(b[j+1]<b[j])
+			{
+				c=b[j+1];
+				b[j+1]=b[j];
+				b[j]=c;
+			}
+		}
+	for(i=0;i<100;i++)
+	{
+		if(b[i]!=0)
+			printf("%d ",b[i]);
+	}
+	return 0;
 }
